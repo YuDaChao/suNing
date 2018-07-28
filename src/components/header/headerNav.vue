fb6bdfee175d612033a644e501d715d7483088d2
happyu <290945266@qq.com>
2018-07-28 21:13:10 +0800

    Initial commit

---
+++ src/components/header/headerNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">头部导航栏</div>
+</template>
+
+<script>
+export default {
+  name: 'headerNav'
+}
+</script>
+
+<style scoped lang="less">
+  .header {
+    font-size: 1rem;
+  }
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <header-nav></header-nav>
+</template>
+
+<script>
+import HeaderNav from '../../components/header/headerNav'
+export default {
+  name: 'index',
+  components: {
+    HeaderNav
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
